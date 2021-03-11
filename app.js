887bce1dd5e8f17231383311d6f90af53b4cd211
Demis Rincon <darmfma@gmail.com>
2021-03-11 11:34:00 -0600

    Feat: Initializaion of app, creation of function to get numbers from user
    Co-authored-by: GONZALO ROJAS <rojasg476@gmail.com>
    Co-authored-by: mrivh <mrivh@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,36 @@
+const CreateBoard = require("./js/CreateBoard")
+
+class LifeGame {
+    constructor(rows, columns) {
+        this.rows = rows;
+        this.columns = columns;
+    }
+
+    RunGame() {
+        const readline = require('readline');
+        const rl = readline.createInterface({
+            input: process.stdin,
+            output: process.stdout
+        });
+
+       
+
+        rl.question('INGRESA FILAS :', (numberRows) => {
+           this.rows = numberRows
+            rl.question('INGRESA COLUMNAS :', (numberColumns) => {
+                this.columns = numberColumns
+
+ 
+                rl.close();
+            });
+        });
+    }
+ 
+    
+
+
+}
+
+let newGame = new LifeGame(2, 2);
+
+newGame.RunGame();
